ce31e09d631c26a6b6fa81fa1ef464a961e2cbe5
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-11-08 12:47:06 +0900

    04_02

--- DirectXCommon.h
+++ DirectXCommon.h
@@ -1,13 +1,70 @@
 
 #pragma once
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl.h>
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
+#include "WinApp.h"
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+using namespace Microsoft::WRL;
+
+
 
 class DirectXCommon
 {
+private:
+	template<class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
 
 public:
 	//初期化
-	void Initialize();
+	void Initialize(WinApp* winApp);
+	//描画前処理
+	void PreDraw();
+	//描画後処理
+	void PostDraw();
+
+private:
+	//デバイス
+	void DeviceInitialize();
+	//コマンド
+	void CommandInitialize();
+	//スワップチェーン
+	void SwapChainInitialize();
+	//レンダターゲット
+	void RenderTargetInitialize();
+	//深度バッファ
+	void DepthBufferTargetInitialize();
+	//フェンス
+	void FenceTargetInitialize();
+
+
+
+private:
+	//WindowsAPI
+	WinApp* winApp = nullptr;
+
+	//DirectX12デバイス
+	Microsoft::WRL::ComPtr<ID3D12Device> device;
+
+	//DXGIファクトリ
+	Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory;
+
+	ComPtr<ID3D12Device> device;
+	ComPtr<IDXGIFactory7> dxgiFactory;
+
 	
+	ComPtr<ID3D12CommandAllocator> commandAllocator;
+	ComPtr<ID3D12GraphicsCommandList> commandList;
+	ComPtr<ID3D12CommandQueue> commandQueue;
 
+	ComPtr<IDXGISwapChain4> swapChain;
 
+	ComPtr<ID3D12DescriptorHeap> rtvHeap;
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
 };
