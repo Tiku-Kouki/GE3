d9a5eb0ebc7c414e3aafd528a5624efb0495aee2
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2024-01-10 12:59:14 +0900

    10回目1:06:56まで

--- Sprite.cpp
+++ Sprite.cpp
@@ -1,6 +1,6 @@
 #include "Sprite.h"
 #include <DirectXMath.h>
-
+#include "BufferResource.h"
 
 using namespace DirectX;
 
@@ -14,50 +14,16 @@ void Sprite::Initialize(DirectXCommon* dxCommon, SpriteCommon* common)
 
 	common_ = common;
 
-	//VertexResource
-	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
-	D3D12_RESOURCE_DESC vertexResourceDesc{};
-	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-	vertexResourceDesc.Width = sizeof(XMFLOAT4) * 3;
-	vertexResourceDesc.Height = 1;
-	vertexResourceDesc.DepthOrArraySize = 1;
-	vertexResourceDesc.MipLevels = 1;
-	vertexResourceDesc.SampleDesc.Count = 1;
-	vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	//頂点 情報 
+	CreateVertex();
 
-	
-	HRESULT result = dxCommon_->GetDevice()->
-		CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE,
-			&vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
-			IID_PPV_ARGS(&vertexResource));
-
-	assert(SUCCEEDED(result));
-
-	//頂点バッファビューを作成する
-	
-	//リソースの先頭のアドレスから使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	//使用するリソースのサイズは頂点3つ分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(XMFLOAT4) * 3;
-	//1頂点あたりのサイズ
-	vertexBufferView.StrideInBytes = sizeof(XMFLOAT4);
+	CreateMaterial();
 
 }
 
 void Sprite::Draw()
 {
-	XMFLOAT4* vertexDate = nullptr;
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexDate));
-
-	//左下	
-	vertexDate[0] = { -0.5f, -0.5f, 0.0f, 1.0f };
 	
-	//上
-	vertexDate[1] = { +0.0f, +0.5f, 0.0f, 1.0f };
-	
-	//右下
-	vertexDate[2] = { +0.5f, -0.5f, 0.0f, 1.0f };
 	
 	dxCommon_->GetCommandList()->SetGraphicsRootSignature(common_->GetRootSignature());
 	dxCommon_->GetCommandList()->SetPipelineState(common_->GetPipelineState());
@@ -65,7 +31,51 @@ void Sprite::Draw()
 	dxCommon_->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView);
 	dxCommon_->GetCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
+	dxCommon_->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
+
 	dxCommon_->GetCommandList()->DrawInstanced(3, 1,0, 0);
 
 
 }
+
+void Sprite::CreateVertex()
+{
+	vertexResource = CreateBufferResource(dxCommon_->GetDevice(), sizeof(XMFLOAT4) * 3);
+
+	//頂点バッファビューを作成する
+
+	//リソースの先頭のアドレスから使う
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	//使用するリソースのサイズは頂点3つ分のサイズ
+	vertexBufferView.SizeInBytes = sizeof(XMFLOAT4) * 3;
+	//1頂点あたりのサイズ
+	vertexBufferView.StrideInBytes = sizeof(XMFLOAT4);
+
+	//頂点 情報
+	XMFLOAT4* vertexDate = nullptr;
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexDate));
+
+	//左下	
+	vertexDate[0] = { -0.5f, -0.5f, 0.0f, 1.0f };
+
+	//上
+	vertexDate[1] = { +0.0f, +0.5f, 0.0f, 1.0f };
+
+	//右下
+	vertexDate[2] = { +0.5f, -0.5f, 0.0f, 1.0f };
+
+}
+
+void Sprite::CreateMaterial()
+{
+	 materialResource= CreateBufferResource(dxCommon_->GetDevice(), sizeof(XMFLOAT4));
+
+	 XMFLOAT4* materialData = nullptr;
+
+	 materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
+
+	
+	materialData = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+
+}
+
--- Sprite.h
+++ Sprite.h
@@ -18,12 +18,20 @@ public:
 	void Draw();
 
 private:
+	void CreateVertex();
+	 //マテリアル情報作成
+	void CreateMaterial();
+
+
+private:
 	DirectXCommon* dxCommon_ = nullptr;
 	SpriteCommon* common_ = nullptr;
-
+	  //頂点情報
 	ComPtr<ID3D12Resource> vertexResource;
 	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
 	
+	//マテリアル情報
+	ComPtr<ID3D12Resource> materialResource
 
 };
 
