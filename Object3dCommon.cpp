466d6874b128ad71daac893883b79edec7930182
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2024-02-14 16:20:37 +0900

    main

--- Object3dCommon.cpp
+++ Object3dCommon.cpp
@@ -3,3 +3,31 @@
 void Object3dCommon::Initialize()
 {
 }
+
+void Object3dCommon::rootSignature()
+{
+    HRESULT result = {};
+
+
+    // ルートシグネチャの設定
+    D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc{};
+    rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+    rootSignatureDesc.pParameters = rootParams; //ルートパラメータの先頭アドレス
+    rootSignatureDesc.NumParameters = _countof(rootParams);        //ルートパラメータ数
+    rootSignatureDesc.pStaticSamplers = &samplerDesc;
+    rootSignatureDesc.NumStaticSamplers = 1;
+    // ルートシグネチャのシリアライズ
+    ID3DBlob* rootSigBlob = nullptr;
+    result = D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1_0, &rootSigBlob, &errorBlob);
+    assert(SUCCEEDED(result));
+    result = device->CreateRootSignature(0, rootSigBlob->GetBufferPointer(), rootSigBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
+    assert(SUCCEEDED(result));
+    rootSigBlob->Release();
+    // パイプラインにルートシグネチャをセット
+    pipelineDesc.pRootSignature = rootSignature.Get();
+    // パイプランステートの生成
+    ComPtr<ID3D12PipelineState> pipelineState;
+    result = device->CreateGraphicsPipelineState(&pipelineDesc, IID_PPV_ARGS(&pipelineState));
+    assert(SUCCEEDED(result));
+
+}
--- Object3dCommon.h
+++ Object3dCommon.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <DirectXMath.h>
+#include <DirectXTex.h>
+#include <d3dcompiler.h>
+
 class Object3dCommon
 {
 public:
 
 	void Initialize();
+
+	void rootSignature();
+
+
 };
 
